b1bebe6e1ec058e475c0d4d8cef3c2ccd4fee393
Harkishan-99 <harkishansinghbaniya@gmail.com>
2021-03-09 21:18:11 +0530

    Added get_data function, logging, order_log and OMS functions

--- strategy.py
+++ strategy.py
@@ -4,19 +4,29 @@ __author__ = 'saeedamen'
 A basic fixed beta trading strategy.
 """
 import os
+import json
+import logging
 import pandas as pd
 
 from utils import _partial_criteria, get_zscore
 from connection import Client
 
-class PairsTrader(Client):
+logging.basicConfig(filename='error.log',
+                    level=logging.WARNING,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
+# setup the connection with the API
+client = Client()
+conn = client.connect()
+api = client.api()
+
+class PairsTrader:
 
     def __init__(self, X:str, Y:str, thresholds:list, lookback_window:int,
-                 base_qty:int, timeframe:str='day'):
+                 base_qty:int, timeframe:str='1D'):
         """
         Pairs trader initialization.
         """
-        super().__init__()
         #check if the assets are active for trading
         self.X = X
         self.Y = Y
@@ -27,19 +37,41 @@ class PairsTrader(Client):
         self.window = lookback_window
         self.base_qty = base_qty
         self.open_positions = False
-        self.filename = "assets.csv"
+        self.open_order = None
+        self.timeframe = timeframe
 
+    def order_log(self, data:dict)->None:
+        with open('orders.json', 'w') as fp:
+            json.dump(data, fp)
 
-    def get_price_data(self, asset:str, limit:int, timeframe:str='D'):
-        pass
+    def get_price_data(self, asset:str, limit:int)->pd.Series:
+        res = api.get_barset(asset, self.timeframe, limit=limit)
+        df = res.df[asset]
+        return df['close']
 
-    def check_criteria(self, X:pd.Series, Y:pd.Series):
+    def check_criteria(self, X:pd.Series, Y:pd.Series)->(bool, pd.Series, float):
         return _partial_criteria(X, Y)
 
-    def OMS(self, asset:str, qty:float):
-        pass
+    def OMS(self, asset:str, qty:float)->None:
+        if qty != 0:
+            if qty > 0:
+                side = 'buy'
+            else:
+                side = 'sell'
 
-    def run(self):
+            resp = api.submit_order(symbol=asset,
+                                    qty=abs(qty),
+                                    side=side)
+            self.order_log(resp)
+        else:
+            try:
+                #close the existing order
+                resp = api.close_position(asset)
+                self.order_log(resp)
+            except Exception as e:
+                logging.exception(e)
+
+    def run(self)->None:
         #get the historical data for X and Y pair
         X = self.get_price_data(self.X, self.window)
         Y = self.get_price_data(self.Y, self.window)
@@ -69,7 +101,9 @@ class PairsTrader(Client):
                     #close the spread postion
                     self.OMS(self.Y, 0)
                     self.OMS(self.X, 0)
+                    self.open_positions = False
         else:
             if self.open_positions:
                 self.OMS(self.Y, 0)
                 self.OMS(self.X, 0)
+                self.open_positions = False
